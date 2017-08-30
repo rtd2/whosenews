e674d0a97d5c01fe3a839c2bad4f94e40a58d030
Robert Thomas <rtd2.v1@gmail.com>
2017-08-29 20:04:28 -0700

    autoplay

--- js/app.js
+++ js/app.js
@@ -5,33 +5,33 @@ function randomStory(storyID) {
     console.log(num);
     var id = storyID;
     var story = stories[id][num];
-    document.getElementById("video").innerHTML = '<iframe id="video" width="1" height="1" src="' + story + '?autoplay=1" frameborder="0"></iframe>';
+    document.getElementById("video").innerHTML = '<iframe id="video" width="1" height="1" src="' + story + '" frameborder="0"></iframe>';
 }
 
 var stories = {
   "northkorea": [
-    "//www.youtube.com/embed/tVqE8XdU7OM",
-    "//www.youtube.com/embed/GzAdcTtwkM0",
-    "//www.youtube.com/embed/SVwW2Rm4LVE",
-    "//www.youtube.com/embed/CryEhQJZ7g0"
+    "//www.youtube.com/embed/tVqE8XdU7OM?autoplay=1",
+    "//www.youtube.com/embed/GzAdcTtwkM0?autoplay=1",
+    "//www.youtube.com/embed/SVwW2Rm4LVE?autoplay=1",
+    "//www.youtube.com/embed/CryEhQJZ7g0?autoplay=1"
   ],
   "trump": [
-    "//www.youtube.com/embed/yGnb7Gy3zEg",
-    "//www.youtube.com/embed/_d2_Upfi3NE",
-    "//www.youtube.com/embed/CC-K5O9EZUE",
-    "//www.youtube.com/embed/opSmUGlvDHA"
+    "//www.youtube.com/embed/yGnb7Gy3zEg?autoplay=1",
+    "//www.youtube.com/embed/_d2_Upfi3NE?autoplay=1",
+    "//www.youtube.com/embed/CC-K5O9EZUE?autoplay=1",
+    "//www.youtube.com/embed/opSmUGlvDHA?autoplay=1"
   ],
   "transtroops": [
-    "//www.youtube.com/embed/3Ikv9JoM5Ss",
-    "//www.youtube.com/embed/coFVPfNkR9A",
-    "//www.youtube.com/embed/AqAwnLPW-mU",
-    "//www.youtube.com/embed/TTUcnEDtNrk"
+    "//www.youtube.com/embed/3Ikv9JoM5Ss?autoplay=1",
+    "//www.youtube.com/embed/coFVPfNkR9A?autoplay=1",
+    "//www.youtube.com/embed/AqAwnLPW-mU?autoplay=1",
+    "//www.youtube.com/embed/TTUcnEDtNrk?autoplay=1"
   ],
   "aca": [
-    "//www.youtube.com/embed/DcHGBaf_ggU",
-    "//www.youtube.com/embed/YnkNVuRmbCo",
-    "//www.youtube.com/embed/kEambA7SaKs",
-    "//www.youtube.com/embed/lYSbz_WPzJA"
+    "//www.youtube.com/embed/DcHGBaf_ggU?autoplay=1",
+    "//www.youtube.com/embed/YnkNVuRmbCo?autoplay=1",
+    "//www.youtube.com/embed/kEambA7SaKs?autoplay=1",
+    "//www.youtube.com/embed/lYSbz_WPzJA?autoplay=1"
   ]
 
 }
